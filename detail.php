54041f0cb1709a47e55c2c5bec88c70ac41df58b
Delio <ddirazar@gmail.com>
2020-04-26 23:54:20 -0300

    token

--- detail.php
+++ detail.php
@@ -45,7 +45,7 @@ require __DIR__ .  '/vendor/autoload.php';
 include_once(__DIR__ ."/access_token.php");
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken( "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398" );
+MercadoPago\SDK::setAccessToken( "APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398" );
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
